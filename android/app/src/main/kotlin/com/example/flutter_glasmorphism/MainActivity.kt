e50f4540782d00b5d0f74f6e69f60e444a7899f1
Jewel Rana <tonujewel@gmail.com>
2021-02-01 14:23:57 +0600

    init project

---
+++ android/app/src/main/kotlin/com/example/flutter_glasmorphism/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_glasmorphism
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FrostedDemo(),
+    );
+  }
+}
+
+
+class FrostedDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          new ConstrainedBox(
+              constraints: const BoxConstraints.expand(),
+              child: new FlutterLogo()
+          ),
+          new Center(
+            child: new ClipRect(
+              child: new BackdropFilter(
+                filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: new Container(
+                  width: 200.0,
+                  height: 200.0,
+                  decoration: new BoxDecoration(
+                      color: Colors.grey.shade200.withOpacity(0.5)
+                  ),
+                  child: new Center(
+                    child: new Text(
+                        'Frosted',
+                        style: Theme.of(context).textTheme.display3
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
