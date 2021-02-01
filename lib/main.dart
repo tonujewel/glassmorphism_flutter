179451b06dbb2e894dc9068288f72632f4031b43
Jewel Rana <tonujewel@gmail.com>
2021-02-01 15:38:49 +0600

    bg image change

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: FrostedDemo(),
@@ -42,12 +30,12 @@ class FrostedDemo extends StatelessWidget {
         children: <Widget>[
           new ConstrainedBox(
               constraints: const BoxConstraints.expand(),
-              child: new FlutterLogo()
+              child: Image.network("https://i.pinimg.com/236x/52/8b/36/528b36f5790c4dbcc43243153174352e.jpg",fit: BoxFit.cover,)
           ),
           new Center(
             child: new ClipRect(
               child: new BackdropFilter(
-                filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                filter: new ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
                 child: new Container(
                   width: 200.0,
                   height: 200.0,
@@ -57,7 +45,7 @@ class FrostedDemo extends StatelessWidget {
                   child: new Center(
                     child: new Text(
                         'Frosted',
-                        style: Theme.of(context).textTheme.display3
+                        style: TextStyle(fontSize: 18)
                     ),
                   ),
                 ),
